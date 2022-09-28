542eda9d58a35c7b6929976273ae3031080c74e0
Eric Conlon <ejconlon@gmail.com>
2022-09-28 13:37:39 -0700

    ghc monad

--- rulecheck/src/Rulecheck/Monad.hs
+++ rulecheck/src/Rulecheck/Monad.hs
@@ -5,51 +5,101 @@ module Rulecheck.Monad
   , newRcEnv
   , RcM (..)
   , runRcM
+  , GhcM
+  , liftGhcM
+  , runGhcM
+  , defaultDynFlags
   ) where
 
 import GHC.Generics (Generic)
-import LittleRIO (HasStateRef (..), SomeRef, RIO, newSomeRef, runRIO)
+import LittleRIO (HasStateRef (..), SomeRef, RIO, newSomeRef, runRIO, readSomeRef)
 import LittleLogger (HasLogAction (..), LogAction, MonadLogger (..), defaultLogAction)
-import Control.Monad.IO.Class (MonadIO)
+import Control.Monad.IO.Class (MonadIO (..))
 import Control.Monad.IO.Unlift (MonadUnliftIO)
-import Control.Monad.Catch (MonadCatch, MonadThrow)
-import Control.Monad.Reader (MonadReader)
-import Control.Monad.State.Strict (MonadState)
+import Control.Monad.Catch (MonadCatch, MonadThrow, MonadMask)
+import Control.Monad.Reader (MonadReader (..), asks)
+import Control.Monad.State.Strict (MonadState (..))
 import Data.Generics.Product (field)
-import GHC (GhcT)
+import GHC (HscEnv, GhcMonad (..), DynFlags, runGhc)
+import GHC.Driver.Session (HasDynFlags (..))
+import GHC.Paths (libdir)
 
+-- RcM - Our application monad
+
+-- Put whatever application state you want here
 data RcState = RcState
   {
-  } deriving (Eq, Show)
+  } deriving (Eq, Show, Generic)
 
+-- Initialize application state here
 newRcState :: RcState
 newRcState = RcState
 
+-- Put whatever application config you want here
 data RcEnv = RcEnv
-  { rcLogAction :: !LogAction
-  , rcStateRef :: !(SomeRef RcState)
+  { reLogAction :: !LogAction
+  , reStateRef :: !(SomeRef RcState)
   } deriving stock (Generic)
 
 instance HasLogAction RcEnv where
-  logActionL = field @"rcLogAction"
+  logActionL = field @"reLogAction"
 
 instance HasStateRef RcState RcEnv where
-  stateRefL = field @"rcStateRef"
+  stateRefL = field @"reStateRef"
 
+-- Initialize application config here
 newRcEnv :: RcState -> IO RcEnv
 newRcEnv s = RcEnv defaultLogAction <$> newSomeRef s
 
 newtype RcM a = RcM { unRcM :: RIO RcEnv a }
-  deriving newtype (Functor, Applicative, Monad, MonadIO, MonadThrow, MonadCatch, MonadUnliftIO, MonadReader RcEnv, MonadLogger, MonadState RcState)
+  deriving newtype (Functor, Applicative, Monad, MonadIO, MonadThrow, MonadCatch, MonadMask, MonadUnliftIO, MonadReader RcEnv, MonadLogger, MonadState RcState)
 
 runRcM :: RcM a -> RcEnv -> IO a
 runRcM = flip runRIO . unRcM
 
-newtype GhcRcM a = GhcRcM { unGhcRcM :: GhcT RcM a }
-  deriving newtype (Functor, Applicative, Monad, MonadIO, MonadThrow, MonadCatch)
+-- GhcM - the GHC monad
 
--- embedRcM :: RcM a -> GhcRcM a
--- embedRcM = GhcRcM . lift
+data GhcEnv r = GhcEnv
+  { geLiftEnv :: !r
+  , geDynFlags :: !DynFlags
+  , geStateRef :: !(SomeRef HscEnv)
+  } deriving stock (Generic)
 
--- instance MonadLogger GhcRcM where
---   monadLoggerLog loc src lvl msg = embedRcM (monadLoggerLog loc src lvl msg)
+instance HasLogAction r => HasLogAction (GhcEnv r) where
+  logActionL = field @"geLiftEnv" . logActionL
+
+instance HasStateRef HscEnv (GhcEnv r) where
+  stateRefL = field @"geStateRef"
+
+newtype GhcM r a = GhcM { unGhcM :: RIO (GhcEnv r) a }
+  deriving newtype (Functor, Applicative, Monad, MonadIO, MonadThrow, MonadCatch, MonadMask, MonadReader (GhcEnv r), MonadState HscEnv, MonadLogger)
+
+instance HasDynFlags (GhcM r) where
+  getDynFlags = asks geDynFlags
+
+instance GhcMonad (GhcM r) where
+  getSession = get
+  setSession = put
+
+liftGhcM :: RIO r a -> GhcM r a
+liftGhcM act = GhcM $ do
+  r <- asks geLiftEnv
+  liftIO (runRIO r act)
+
+runGhcM :: DynFlags -> GhcM r a -> RIO r a
+runGhcM dynFlags act = do
+  r <- ask
+  liftIO $ do
+    runGhc (Just libdir) $ do
+      sess <- getSession
+      sr <- newSomeRef sess
+      let ge = GhcEnv r dynFlags sr
+      ret <- liftIO (runRIO ge (unGhcM act))
+      -- note sure if we really need to write the session back, but we can
+      sess' <- readSomeRef sr
+      setSession sess'
+      pure ret
+
+-- TODO this is a LARGE struct - come up with some good defaults
+defaultDynFlags :: DynFlags
+defaultDynFlags = error "TODO"
