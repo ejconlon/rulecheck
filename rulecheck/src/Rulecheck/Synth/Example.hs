48dd58e419062ed80763cb359f950dac6779209d
Eric Conlon <ejconlon@gmail.com>
2022-10-21 16:55:23 -0700

    start logict search

--- rulecheck/src/Rulecheck/Synth/Example.hs
+++ rulecheck/src/Rulecheck/Synth/Example.hs
@@ -4,12 +4,9 @@ module Rulecheck.Synth.Example where
 
 import Data.Map.Strict (Map)
 import Data.Sequence (Seq (..))
-import IntLike.Map (IntLikeMap)
-import IntLike.Set (IntLikeSet)
-import Rulecheck.Synth.Align (TyUniq)
-import Rulecheck.Synth.Core (Scheme (..), Tm (..), TmName, Ty (..))
+import Rulecheck.Synth.Core (Index (..), Scheme (..), Tm (..), TmName, Ty (..))
 import Rulecheck.Synth.Decl (Decl, DeclErr, mkDecls)
-import Rulecheck.Synth.Search (TmUnify, TmUniq, outerSearch)
+import Rulecheck.Synth.Search (SearchConfig (..), runSearchN)
 
 exampleDecls :: Either (TmName, DeclErr) (Map TmName Decl)
 exampleDecls = res where
@@ -23,8 +20,8 @@ exampleDecls = res where
     , ("myPlus", Scheme Empty tyIntFun2, Just (TmLam "a" (TmLam "b" (TmApp (TmApp (TmKnown "+") tmA) tmB))))
     ]
 
-exampleSearch :: IO ([TmUniq], IntLikeMap TyUniq (IntLikeSet TmUniq), IntLikeMap TmUniq TmUnify)
-exampleSearch = do
+exampleSearch :: Int -> IO [Tm Index]
+exampleSearch n = do
   let scheme = Scheme mempty (TyCon "Int" mempty)
   decls <- either (\p -> fail ("Decl err: " ++ show p)) pure exampleDecls
-  outerSearch decls scheme 5
+  runSearchN (SearchConfig decls scheme 5) n
--- rulecheck/src/Rulecheck/Synth/Search.hs
+++ rulecheck/src/Rulecheck/Synth/Search.hs
@@ -1,12 +1,13 @@
 module Rulecheck.Synth.Search where
 
+import Control.Applicative (Alternative)
 import Control.Monad (when)
-import Control.Monad.IO.Class (MonadIO (..))
+import Control.Monad.Identity (Identity (..))
+import Control.Monad.Logic (LogicT, MonadLogic (..), observeManyT)
 import Control.Monad.Reader (MonadReader (..), ReaderT (..), asks)
-import Control.Monad.State.Strict (MonadState (..), StateT (..))
+import Control.Monad.State.Strict (MonadState (..), State, StateT (..))
 import Data.Foldable (foldl', for_, toList)
 import Data.Functor.Foldable (cata, project)
-import Data.IORef (modifyIORef', newIORef, readIORef, writeIORef)
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
 import Data.Sequence (Seq (..))
@@ -15,7 +16,7 @@ import IntLike.Map (IntLikeMap)
 import qualified IntLike.Map as ILM
 import IntLike.Set (IntLikeSet)
 import Rulecheck.Synth.Align (TyUnify, TyUniq (..), TyVert (..), mightAlign, recAlignTys)
-import Rulecheck.Synth.Core (Index (..), Scheme (..), TmF, TmName, TyF (..))
+import Rulecheck.Synth.Core (Index (..), Scheme (..), Tm, TmF, TmName, TyF (..))
 import Rulecheck.Synth.Decl (Decl (..))
 import Rulecheck.Synth.UnionMap (UnionMap)
 import qualified Rulecheck.Synth.UnionMap as UM
@@ -43,54 +44,61 @@ data St = St
   , stTms :: !(IntLikeMap TmUniq TmUnify)
   } deriving stock (Eq, Show)
 
-newtype SearchM a = SearchM { unSearchM :: ReaderT Env (StateT St IO) a }
-  deriving newtype (Functor, Applicative, Monad, MonadReader Env, MonadState St, MonadIO)
-
-runSearchM :: SearchM a -> Env -> St -> IO (a, St)
-runSearchM m env = runStateT (runReaderT (unSearchM m) env)
+newtype SearchM a = SearchM { unSearchM :: LogicT (ReaderT Env (State St)) a }
+  deriving newtype (Functor, Applicative, Monad, Alternative, MonadReader Env, MonadState St, MonadLogic)
 
 toListWithIndex :: Seq a -> [(a, Index)]
 toListWithIndex ss = zip (toList ss) (fmap Index [Seq.length ss - 1 .. 0])
 
-innerSearch :: (Env -> SearchM ()) -> (TmUniq -> SearchM ()) -> SearchM ()
-innerSearch _nominate _answer = do
-  Env decls ctx _ _ goalKey goalVal _<- ask
-  -- Go through all variables in context to find something that might fit the hole
-  for_ (toListWithIndex ctx) $ \(candKey, _idx) -> do
-    um <- asks envTys
-    case recAlignTys goalKey candKey um of
-      Left _ -> pure ()
-      Right (_u, _) -> do
-        -- TODO fill in var
-        error "TODO"
-  -- Go through all known decls to find something that might fit this hole exactly
-  for_ (Map.toList decls) $ \(_name, decl) -> do
-    -- Do a cheap check on the outside to see if it might align
-    let candVal = project (schemeBody (declScheme decl))
-    when (mightAlign goalVal candVal) $ do
-      -- liftIO (putStrLn ("Might align: " ++ show candVal))  -- XXX
-      insertingTy (declScheme decl) $ \candKey -> do
-        um <- asks envTys
-        case recAlignTys goalKey candKey um of
-          Left _ -> pure ()
-          Right (_u, _um') -> do
-            -- * add term to map
-            -- * if parents is empty (this is top), emit an answer
-            --   this answer will need to be checked later
-            --   because we'll only know if search successfully filled out all subterms
-            --   eventually use a dependency map to emit only finished stuff
-            -- * lookup the ty and nominate envs for children
-            --   (with appropriate goals, depth, and parents)
-            error "TODO"
-      pure ()
-  -- TODO go through all function decls to find something that might fit the hole
-  -- given some arguments
+adaptSearch :: TmUniq -> SearchM (Tm Index)
+adaptSearch = error "TODO"
+
+innerSearch :: SearchM TmUniq
+innerSearch = do
+  error "TODO"
+  -- Env decls ctx _ _ goalKey goalVal _ <- ask
+  -- -- Go through all variables in context to find something that might fit the hole
+  -- for_ (toListWithIndex ctx) $ \(candKey, _idx) -> do
+  --   um <- asks envTys
+  --   case recAlignTys goalKey candKey um of
+  --     Left _ -> pure ()
+  --     Right (_u, _) -> do
+  --       -- TODO fill in var
+  --       error "TODO"
+  -- -- Go through all known decls to find something that might fit this hole exactly
+  -- for_ (Map.toList decls) $ \(_name, decl) -> do
+  --   -- Do a cheap check on the outside to see if it might align
+  --   let candVal = project (schemeBody (declScheme decl))
+  --   when (mightAlign goalVal candVal) $ do
+  --     -- liftIO (putStrLn ("Might align: " ++ show candVal))  -- XXX
+  --     insertingTy (declScheme decl) $ \candKey -> do
+  --       um <- asks envTys
+  --       case recAlignTys goalKey candKey um of
+  --         Left _ -> pure ()
+  --         Right (_u, _um') -> do
+  --           -- * add term to map
+  --           -- * if parents is empty (this is top), emit an answer
+  --           --   this answer will need to be checked later
+  --           --   because we'll only know if search successfully filled out all subterms
+  --           --   eventually use a dependency map to emit only finished stuff
+  --           -- * lookup the ty and nominate envs for children
+  --           --   (with appropriate goals, depth, and parents)
+  --           error "TODO"
+  --     pure ()
+  -- -- TODO go through all function decls to find something that might fit the hole
+  -- -- given some arguments
 
 insertingTy :: Scheme Index -> (TyUniq -> SearchM ()) -> SearchM ()
 insertingTy _scheme onAdded = do
   let u = error "TODO"
   let env' = error "TODO"
   local (const env') (onAdded u)
+
+data SearchConfig = SearchConfig
+  { scDecls :: !(Map TmName Decl)
+  , scTarget :: !(Scheme Index)
+  , scDepthLim :: !Int
+  } deriving stock (Eq, Show)
 
 -- just a helper for the cata
 runReaderStateM :: r -> s -> ReaderT r (StateT s m) a -> m (a, s)
@@ -104,8 +112,8 @@ lookupCtx i = do
     Nothing -> fail ("Missing index " ++ show (unIndex i))
     Just x -> pure x
 
-initEnvSt :: MonadFail m => Map TmName Decl -> Scheme Index -> Int -> m (Env, St)
-initEnvSt decls (Scheme tvs ty) depthLim = do
+initEnvSt :: MonadFail m => SearchConfig -> m (Env, St)
+initEnvSt (SearchConfig decls (Scheme tvs ty) depthLim) = do
   let (msrc, ctx) = foldl' (\((mx, srcx), ctxx) tv -> ((ILM.insert srcx (TyVertSkolem tv) mx, srcx + 1), ctxx :|> srcx)) ((ILM.empty, 0), Seq.empty) tvs
       insert v = do
         (m, src) <- get
@@ -126,28 +134,31 @@ initEnvSt decls (Scheme tvs ty) depthLim = do
       st = St src' ILM.empty 0 ILM.empty
   pure (env, st)
 
-outerSearch :: Map TmName Decl -> Scheme Index -> Int -> IO ([TmUniq], IntLikeMap TyUniq (IntLikeSet TmUniq), IntLikeMap TmUniq TmUnify)
-outerSearch decls scheme depthLim = go where
-  go = do
-    (env, st) <- initEnvSt decls scheme depthLim
-    nomRef <- newIORef ([] :: [Env])
-    ansRef <- newIORef ([] :: [TmUniq])
-    stRef <- newIORef st
-    let m = innerSearch (nominate nomRef) (answer ansRef)
-    _ <- loop nomRef stRef env m
-    finals <- liftIO (readIORef ansRef)
-    tyReps <- fmap stTyReps (readIORef stRef)
-    tms <- fmap stTms (readIORef stRef)
-    pure (finals, tyReps, tms)
-  loop nomRef stRef env m = do
-    st <- readIORef stRef
-    ((), st') <- runSearchM m env st
-    writeIORef stRef st'
-    noms <- readIORef nomRef
-    case noms of
-      [] -> pure ()
-      env':envs -> do
-        writeIORef nomRef envs
-        loop nomRef stRef env' m
-  nominate nomRef env = liftIO (modifyIORef' nomRef (env:))
-  answer ansRef ans = liftIO (modifyIORef' ansRef (ans:))
+data SearchSusp a = SearchSusp
+  { ssEnv :: !Env
+  , ssSt :: !St
+  , ssAct :: !(SearchM a)
+  }
+
+nextSearchResult :: SearchSusp a -> Maybe (a, SearchSusp a)
+nextSearchResult (SearchSusp env st act) =
+  let (xs, st') = runIdentity (runReaderStateM env st (observeManyT 1 (msplit (unSearchM act))))
+  in case xs of
+    [] -> Nothing
+    x:_ ->
+      case x of
+        Nothing -> Nothing
+        Just (a, act') -> Just (a, SearchSusp env st' (SearchM act'))
+
+runSearch :: MonadFail m => SearchConfig -> m (SearchSusp (Tm Index))
+runSearch sc = do
+  (env, st) <- initEnvSt sc
+  let act = innerSearch >>= adaptSearch
+  pure (SearchSusp env st act)
+
+runSearchN :: MonadFail m => SearchConfig -> Int -> m [Tm Index]
+runSearchN sc n = do
+  (env, st) <- initEnvSt sc
+  let act = innerSearch >>= adaptSearch
+  let (xs, _) = runIdentity (runReaderStateM env st (observeManyT n (unSearchM act)))
+  pure xs
