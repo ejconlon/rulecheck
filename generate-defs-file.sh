47c8684b29b9f307dfbce086d685be3badd6fd39
Zack Grannan <zgrannan@protonmail.com>
2022-12-06 11:59:20 -0800

    New def gen code

--- generate-defs-file.sh
+++ generate-defs-file.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env sh
+#
+# Note: Tested using Cabal version 3.8.1.0
+# Older versions may not work!!!
 
 set -euo pipefail
 
-HOOGLE_FILE=$(cabal haddock --haddock-hoogle | tail -n1)
+HADDOCK=$(which haddock)
 
+HOOGLE_FILE=$(cabal haddock --with-haddock=$HADDOCK --with-ghc=ghc-9.0.2 --haddock-hoogle | tail -n1)
+
+# sed '/-> \*/d' is a heuristic for eliminating things w/ higher kinded types
+# sed '/forall.*forall/d eliminates stuff w/ nested foralls
 # The command sed 's/^\[\(.*\)\]/\1/g' removes replaces [ID] with ID for haddock hidden syntax
-sed '/^--/d' "$HOOGLE_FILE" | sed '/^@/d' | sed '/^infixl /d' |\
-    sed 's/^\[\(.*\)\]/\1/g' > defs.txt
+sed '/^--/d' "$HOOGLE_FILE" | \
+    sed '/-> \*/d'|\
+    sed '/forall.*forall/d'|\
+    sed 's/^newtype /data /' | sed 's/^\[\(.*\)\]/\1/g' > defs.txt
